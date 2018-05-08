24fa3d63e0f0dc4a9739807ba5dd132f5d7d07f1
Angelina Wong <angelina.w25@gmail.com>
2018-05-08 18:26:51 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,13 @@
 def my_each(word) # put argument(s) here
   counter = 0
-  
+
   while n < array.length
     my_each(word) do |word|
-      
+
       yield word
-      
+
     counter += 1
-    
+
       end
   end
 end
